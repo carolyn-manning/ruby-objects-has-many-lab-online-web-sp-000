30c159c465127334889e1fbcd2f9d225b3a8e1d5
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-25 02:55:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,7 @@
 class Artist
 
   attr_accessor :name
+  @songs = []
 
   def initialize(name)
     @name = name
@@ -11,6 +12,7 @@ class Artist
   end
 
   def add_song(song)
+    @songs << song
     song.artist = self
   end
 
@@ -20,7 +22,6 @@ class Artist
   end
 
   def self.song_count
-    songs.count
   end
 
 end
